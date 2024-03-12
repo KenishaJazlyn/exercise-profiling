4e49ed5c5e7445a0833b9a7700e95238f03d2934
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-03-12 23:23:35 +0700

    [Refactoring] - Optimizing method findStudentWithHighestGpa

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 /**
  * @author muhammad.khadafi
  */
@@ -28,16 +29,9 @@ public class StudentService {
     }
 
     public Optional<Student> findStudentWithHighestGpa() {
-        List<Student> students = studentRepository.findAll();
-        Student highestGpaStudent = null;
-        double highestGpa = 0.0;
-        for (Student student : students) {
-            if (student.getGpa() > highestGpa) {
-                highestGpa = student.getGpa();
-                highestGpaStudent = student;
-            }
-        }
-        return Optional.ofNullable(highestGpaStudent);
+        Pageable limit = PageRequest.of(0, 1);
+        List<Student> topStudents = studentRepository.getHighestGpa(limit);
+        return topStudents.stream().findFirst();
     }
 
     public String joinStudentNames() {
